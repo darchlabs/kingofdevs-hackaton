e06fe57da7f84303aace995496d9d7218cd09e4e
Camilo Acuña <camilo.aku@gmail.com>
2023-04-01 05:45:40 +0700

    feat(frontend): add get-icon-by-section util method

---
+++ frontend/app/utils/get-icon-by-section.tsx
@@ -0,0 +1,17 @@
+import { VscOrganization, VscPieChart, VscSettingsGear } from "react-icons/vsc";
+import { HiOutlineDocumentText } from "react-icons/hi";
+
+export const getIconBySection = (section: string) => {
+  switch (section.toLowerCase()) {
+    case "metrics":
+      return <VscPieChart size={25} />;
+    case "smartcontracts":
+      return <HiOutlineDocumentText size={25} />;
+    case "users":
+      return <VscOrganization size={25} />;
+    case "settings":
+      return <VscSettingsGear size={25} />;
+  }
+
+  return null;
+};
